ca7112f36426a8b55337e0611541a2fa6ff19315
zackdk <958059970@qq.com>
2018-09-14 22:57:55 +0800

    完善关于 增加启动页面

--- app/src/main/java/com/zack/article/Activity/AboutActivity.java
+++ app/src/main/java/com/zack/article/Activity/AboutActivity.java
@@ -1,14 +1,24 @@
 package com.zack.article.Activity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zack.article.R;
+import com.zack.article.Util.CountEventHelper;
 
-public class AboutActivity extends BaseActivity {
+import java.net.URISyntaxException;
+
+public class AboutActivity extends BaseActivity implements View.OnClickListener {
     private Toolbar toolbar;
+    private TextView qq,email,pay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +34,97 @@ public class AboutActivity extends BaseActivity {
                 finish();
             }
         });
+        qq.setOnClickListener(this);
+        email.setOnClickListener(this);
+        pay.setOnClickListener(this);
     }
 
     private void initView() {
         toolbar = findViewById(R.id.toolbar);
+        qq = findViewById(R.id.qq);
+        email = findViewById(R.id.email);
+        pay = findViewById(R.id.pay);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.qq:
+                if(!joinQQGroup("wL36iJryOophM7iZDEnjHnYnWDw50h4n")){
+                    toast("未安装手Q或安装的版本不支持");
+                }
+                break;
+            case R.id.email:
+                email();
+                break;
+            case R.id.pay:
+                openALiPay();
+                break;
+        }
+    }
+    /****************
+     *
+     * 发起添加群流程。群号：空山(793272974) 的 key 为： wL36iJryOophM7iZDEnjHnYnWDw50h4n
+     * 调用 joinQQGroup(wL36iJryOophM7iZDEnjHnYnWDw50h4n) 即可发起手Q客户端申请加群 空山(793272974)
+     *
+     * @param key 由官网生成的key
+     * @return 返回true表示呼起手Q成功，返回fals表示呼起失败
+     ******************/
+    public boolean joinQQGroup(String key) {
+        Intent intent = new Intent();
+        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            startActivity(intent);
+            return true;
+        } catch (Exception e) {
+            // 未安装手Q或安装的版本不支持
+            return false;
+        }
+    }
+    public  boolean hasInstalledAlipayClient() {
+        String ALIPAY_PACKAGE_NAME = "com.eg.android.AlipayGphone";
+        PackageManager pm = this.getApplicationContext().getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(ALIPAY_PACKAGE_NAME, 0);
+            return info != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void openALiPay(){
+        CountEventHelper.countAlipay(this);
+        String url1="intent://platformapi/startapp?saId=10000007&" +
+                "clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2Fa6x05048iewv4tsdnwmykbc%3F_s" +
+                "%3Dweb-other&_t=1472443966571#Intent;" +
+                "scheme=alipayqr;package=com.eg.android.AlipayGphone;end";
+        //String url1=activity.getResources().getString(R.string.alipay);
+        Intent intent = null;
+        Toast.makeText(this.getApplicationContext(),"感谢您的捐赠！٩(๑❛ᴗ❛๑)۶",Toast.LENGTH_SHORT).show();
+        if(hasInstalledAlipayClient()){
+            try {
+                intent = Intent.parseUri(url1 ,Intent.URI_INTENT_SCHEME );
+                startActivity(intent);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                Toast.makeText(this.getApplicationContext(),"出错啦",Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(this.getApplicationContext(),"您未安装支付宝哦！(>ω･* )ﾉ",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void email(){
+        CountEventHelper.countOpenEmail(this);
+        Uri uri = Uri.parse("mailto:958059970@qq.com");
+        String[] email = {"958059970@qq.com"};
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        intent.putExtra(Intent.EXTRA_CC, email); // 抄送人
+        intent.putExtra(Intent.EXTRA_SUBJECT, "初见--软件的意见反馈"); // 主题
+        intent.putExtra(Intent.EXTRA_TEXT, ""); // 正文
+        startActivity(Intent.createChooser(intent, "请选择邮件类应用"));
     }
 
 }
