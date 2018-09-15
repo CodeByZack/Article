50e0f99bd709ddad74caa4d8ff53188ab8b4d612
zackdk <958059970@qq.com>
2018-09-15 12:45:43 +0800

    创建应用信息

--- app/src/main/java/com/zack/article/Activity/SplashActivity.java
+++ app/src/main/java/com/zack/article/Activity/SplashActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.zack.article.R;
+import com.zack.article.Util.PackageUtil;
 
 public class SplashActivity extends BaseActivity {
 
@@ -13,6 +15,10 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        String Ver = "遇见~      版本号：v"+PackageUtil.packageName(this);
+        TextView version = findViewById(R.id.version);
+        version.setText(Ver);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
